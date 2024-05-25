19ec915442b0ad473a5d3634ce649be2e0774997
valeelim <valeriansalim@gmail.com>
2024-05-25 16:16:19 +0700

    finished function scoping

---
+++ src/main/java/org/vlrnlm/tool/GenerateAst.java
@@ -0,0 +1,105 @@
+package org.vlrnlm.tool;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+
+class GenerateAst {
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.err.println("Usage: generate_ast <output directory>");
+            System.exit(64);
+        }
+        String outputDir = args[0];
+        defineAst(outputDir, "Expr", Arrays.asList(
+                "Assign : Token name, Expr value",
+                "Binary : Expr left, Token operator, Expr right",
+                "Call : Expr callee, Token paren, List<Expr> arguments",
+                "Grouping : Expr expression",
+                "Literal : Object value",
+                "Logical : Expr left, Token operator, Expr right",
+                "Unary : Token operator, Expr right",
+                "Variable : Token name"
+        ));
+
+        /**
+         * program      -> declaration* EOF ;
+         * declaration  -> varDecl | statement ;
+         * statement    -> exprStmt | printStmt ;
+         * varDecl      -> "var" IDENTIFIER ( "=" expression )? ";" ;
+         */
+        defineAst(outputDir, "Stmt", Arrays.asList(
+                "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
+                "Block : List<Stmt> statements",
+                "Expression : Expr expression",
+                "Function : Token name, List<Token> params, List<Stmt> body",
+                "Print : Expr expression",
+                "Return : Token keyword, Expr value",
+                "Var : Token name, Expr initializer",
+                "While : Expr condition, Stmt body"
+        ));
+    }
+
+    private static void defineAst(String outputDir, String baseName, List<String> types) throws IOException {
+        String path = outputDir + "/" + baseName + ".java";
+        PrintWriter writer = new PrintWriter(path, StandardCharsets.UTF_8);
+
+        writer.println("package org.vlrnlm.lox;");
+        writer.println();
+        writer.println("import java.util.List;");
+        writer.println();
+        writer.println("abstract class " + baseName + " {");
+
+        defineVisitor(writer, baseName, types);
+
+        for (String type : types) {
+            String className = type.split(":")[0].trim();
+            String fields = type.split(":")[1].trim();
+            defineType(writer, baseName, className, fields);
+        }
+
+        writer.println();
+        writer.println("    abstract <R> R accept(Visitor <R> visitor);");
+
+        writer.println("}");
+        writer.close();
+    }
+
+    private static void defineVisitor(PrintWriter writer, String baseName, List<String>types) {
+        writer.println("    interface Visitor<R> {");
+
+        for (String type : types) {
+            String typeName = type.split(":")[0].trim();
+            writer.println("        R visit" + typeName + baseName + "(" + typeName + " " + baseName.toLowerCase() + ");");
+        }
+
+        writer.println("    }");
+    }
+
+    private static void defineType(PrintWriter writer, String baseName, String className, String fieldList) {
+        writer.println("    static class " + className + " extends " + baseName + " {");
+        writer.println("        " + className + "(" + fieldList + ") {");
+
+        String[] fields = fieldList.split(", ");
+        for (String field : fields) {
+            String name = field.split(" ")[1];
+            writer.println("            this." + name + " = " + name + ";");
+        }
+
+        writer.println("        }");
+
+        writer.println();
+        writer.println("        @Override");
+        writer.println("        <R> R accept(Visitor<R> visitor) {");
+        writer.println("            return visitor.visit" + className + baseName + "(this);");
+        writer.println("        }");
+
+        writer.println();
+        for (String field : fields) {
+            writer.println("        final " + field + ";");
+        }
+        writer.println("    }");
+    }
+}
