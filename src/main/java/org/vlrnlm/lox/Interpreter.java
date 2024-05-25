c29c34d501f83223bdc174f799002b7f4aad333d
valeelim <valeriansalim@gmail.com>
2024-05-25 18:48:05 +0700

    add semantic analysis

--- src/main/java/org/vlrnlm/lox/Interpreter.java
+++ src/main/java/org/vlrnlm/lox/Interpreter.java
@@ -1,13 +1,16 @@
 package org.vlrnlm.lox;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.vlrnlm.lox.TokenType.*;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     final Environment globals = new Environment();
     private Environment environment = globals;
+    private final Map<Expr, Integer> locals = new HashMap<>();
 
     Interpreter() {
         globals.define("clock", new LoxCallable() {
@@ -41,6 +44,10 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     private void execute(Stmt statement) {
         statement.accept(this);
+    }
+
+    void resolve(Expr expr, int depth) {
+        locals.put(expr, depth);
     }
 
     private String stringify(Object object) {
@@ -147,7 +154,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     @Override
     public Object visitAssignExpr(Expr.Assign expr) {
         Object value = evaluate(expr.value);
-        environment.assign(expr.name, value);
+
+        Integer distance = locals.get(expr);
+        if (distance != null) {
+            environment.assignAt(distance, expr.name, value);
+        }
+        else {
+            globals.assign(expr.name, value);
+        }
         return value;
     }
 
@@ -164,7 +178,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
     @Override
     public Object visitVariableExpr(Expr.Variable expr) {
-        return environment.get(expr.name);
+        return lookUpVariable(expr.name, expr);
     }
 
     @Override
@@ -251,6 +265,13 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
             }
             default -> null;
         };
+    }
+
+    private Object lookUpVariable(Token name, Expr expr) {
+        Integer distance = locals.get(expr);
+        if (distance != null)
+            return environment.getAt(distance, name.lexeme);
+        return globals.get(name);
     }
 
     public void checkNumberOperand(Token operator, Object operand) {
