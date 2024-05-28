55f6726e857f916f6e648a353150e9a4f132657b
valeelim <valeriansalim@gmail.com>
2024-05-28 11:21:47 +0700

    add inheritance support on classes

--- src/main/java/org/vlrnlm/lox/Expr.java
+++ src/main/java/org/vlrnlm/lox/Expr.java
@@ -13,6 +13,7 @@ abstract class Expr {
         R visitLiteralExpr(Literal expr);
         R visitLogicalExpr(Logical expr);
         R visitSetExpr(Set expr);
+        R visitSuperExpr(Super expr);
         R visitUnaryExpr(Unary expr);
         R visitVariableExpr(Variable expr);
     }
@@ -143,6 +144,20 @@ abstract class Expr {
         final Expr object;
         final Token name;
         final Expr value;
+    }
+    static class Super extends Expr {
+        Super(Token keyword, Token method) {
+            this.keyword = keyword;
+            this.method = method;
+        }
+
+        @Override
+        <R> R accept(Visitor<R> visitor) {
+            return visitor.visitSuperExpr(this);
+        }
+
+        final Token keyword;
+        final Token method;
     }
     static class Unary extends Expr {
         Unary(Token operator, Expr right) {
--- src/main/java/org/vlrnlm/tool/GenerateAst.java
+++ src/main/java/org/vlrnlm/tool/GenerateAst.java
@@ -23,6 +23,7 @@ class GenerateAst {
                 "Literal : Object value",
                 "Logical : Expr left, Token operator, Expr right",
                 "Set : Expr object, Token name, Expr value",
+                "Super : Token keyword, Token method",
                 "Unary : Token operator, Expr right",
                 "Variable : Token name"
         ));
@@ -36,7 +37,7 @@ class GenerateAst {
         defineAst(outputDir, "Stmt", Arrays.asList(
                 "If : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Block : List<Stmt> statements",
-                "Class : Token name, List<Stmt.Function> methods",
+                "Class : Token name, Expr.Variable superclass, List<Stmt.Function> methods",
                 "Expression : Expr expression",
                 "Function : Token name, List<Token> params, List<Stmt> body",
                 "Print : Expr expression",
